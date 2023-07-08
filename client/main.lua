b1d3ac649766eadb5f22f8833597723e247b8285
Sqrl <68661127+Sqrl34@users.noreply.github.com>
2023-07-08 15:19:38 -0700

    Add files via upload

---
+++ client/main.lua
@@ -0,0 +1,250 @@
+QBCore = exports['qb-core']:GetCoreObject()
+local inprogress = false
+local currentBlip
+local currentObject
+local pickup = false
+local indx
+local finnishedCooldown = true
+local itemsGiven = false
+local drugZone
+
+
+
+-- functions
+local function createBlips()
+    local starterBlip = AddBlipForCoord(Config.Blip.location.x, Config.Blip.location.y, Config.Blip.location.z)
+    SetBlipColour(starterBlip, Config.Blip.color)
+    SetBlipScale(starterBlip, Config.Blip.scale)
+    SetBlipSprite(starterBlip, Config.Blip.sprite)
+    SetBlipDisplay(starterBlip, 4)
+    BeginTextCommandSetBlipName("STRING")
+    AddTextComponentSubstringPlayerName(Config.Blip.name)
+    EndTextCommandSetBlipName(starterBlip)
+end
+
+local function createTargetPed()
+    local model = Config.StarterPedInfo.model
+    RequestModel(model)
+    while not HasModelLoaded(model) do
+        Wait(0)
+    end
+    local ped = CreatePed(0, model, Config.StarterPedInfo.coords.x, Config.StarterPedInfo.coords.y,
+        Config.StarterPedInfo.coords.z, Config.StarterPedInfo.coords.w, true, false)
+    FreezeEntityPosition(ped, true)
+    exports['qb-target']:AddTargetEntity(ped, {
+        options = {
+            {
+                type = "client",
+                event = "quickcash:client:startProcess",
+                icon = 'fas fa-clipboard',
+                label = "Talk to Oswald about a special project",
+            }
+        },
+        distance = 2.5,
+    })
+end
+
+local function createUsefulBlip(sprite, color, name, location)
+    print(currentBlip)
+    currentBlip = nil
+    print(currentBlip)
+    currentBlip = AddBlipForCoord(location.x, location.y, location.z)
+    SetBlipColour(currentBlip, color)
+    SetBlipScale(currentBlip, .6)
+    SetBlipSprite(currentBlip, sprite)
+    SetBlipDisplay(currentBlip, 4)
+    SetBlipRoute(currentBlip, true)
+    SetBlipRouteColour(currentBlip, 16)
+    BeginTextCommandSetBlipName("STRING")
+    AddTextComponentSubstringPlayerName(name)
+    EndTextCommandSetBlipName(currentBlip)
+end
+
+local function sendMail(send, subjects, messages)
+    TriggerServerEvent('qb-phone:server:sendNewMail', {
+        sender = send,
+        subject = subjects,
+        message = messages,
+        button = {}
+    })
+end
+
+local function fillInventory(item, amount)
+    TriggerServerEvent('quickcash:server:fill', item, amount)
+    itemsGiven = true
+end
+
+local function doModel(newmodel, location, message, item, amount)
+    local model = newmodel
+    RequestModel(model)
+    while not HasModelLoaded(model) do
+        Wait(0)
+    end
+    currentObject = CreateObject(model, location.x, location.y, location.z, true, true, false)
+
+    exports['qb-target']:AddTargetEntity(currentObject, {
+        options = {
+            {
+                icon = 'fas fa-clipboard',
+                label = 'Pick up the model',
+                action = function()
+                    sendMail('Oswald', 'Drop Location', message)
+                    DeleteObject(currentObject)
+                    fillInventory(item, amount)
+                    RemoveBlip(currentBlip)
+                    pickup = true
+                end
+            }
+        },
+        distance = 2.5,
+    })
+end
+
+
+
+
+local function chooseIllegal()
+    local place = math.random(1, #Config.Locations)
+    indx = place
+    createUsefulBlip(Config.Locations[place].Blip.sprite, Config.Locations[place].Blip.color,
+        Config.Locations[place].Blip.name, Config.Locations[place].Blip.coords)
+    sendMail("Osawald", "Shipment", Config.Locations[place].Messages.Pickup)
+    doModel(Config.Locations[place].Prop.model, Config.Locations[place].Prop.coords,
+        Config.Locations[place].Messages.Drop, Config.Locations[place].Item, Config.Locations[place].Amount)
+end
+
+local function menuDeliver()
+    local deliverMenu = {
+        {
+            header = "Deliver Options",
+            isMenuHeader = true
+        }
+    }
+
+    deliverMenu[#deliverMenu + 1] = {
+        header = "Place object on delivery location",
+        params = {
+            event = 'quickcash:client:serverTrigger',
+            args = {
+                item = Config.Locations[indx].Item,
+                amount = Config.Locations[indx].Amount,
+                reward = Config.Locations[indx].Reward,
+                finished = true
+            }
+        }
+    }
+
+    deliverMenu[#deliverMenu + 1] = {
+        header = "Close Menu",
+        params = {
+            event = "qb-menu:client:closeMenu"
+        }
+    }
+
+    inprogress = false
+
+    exports['qb-menu']:openMenu(deliverMenu)
+end
+
+local function closeMenu()
+    exports['qb-menu']:closeMenu()
+end
+
+local function finnishIllegal()
+    local location = math.random(1, #Config.DropOff.Locations)
+    createUsefulBlip(Config.DropOff.Blip.sprite, Config.DropOff.Blip.color, Config.DropOff.Blip.name,
+        Config.DropOff.Locations[location].coords)
+
+    drugZone = BoxZone:Create(
+    vector3(Config.DropOff.Locations[location].coords.x, Config.DropOff.Locations[location].coords.y,
+        Config.DropOff.Locations[location].coords.z), 3.0, 5.0, {
+        name = "deliver_zone",
+        minz = Config.DropOff.Locations[location].coords.z - 2.0,
+        maxz = Config.DropOff.Locations[location].coords.z + 2.0,
+        debugPoly = false,
+    })
+
+    drugZone:onPlayerInOut(function(isPointInside)
+        if isPointInside then
+            menuDeliver()
+        else
+            closeMenu()
+        end
+    end)
+end
+
+local function cooldown()
+    local time = Config.Cooldown * 60 * 1000
+    finnishedCooldown = false
+    Wait(time)
+    finnishedCooldown = true
+
+end
+
+-- Events
+RegisterNetEvent('quickcash:client:startProcess', function()
+    if finnishedCooldown then
+        if not inprogress then
+            inprogress = not inprogress
+            chooseIllegal()
+            while not pickup do
+                Wait(1000)
+            end
+            finnishIllegal()
+            pickup = false
+        else
+            QBCore.Functions.Notify('You already have a job in progress.', 'error', 5000)
+        end
+    else
+        QBCore.Functions.Notify('You have a cooldown running, take a brake', 'error', 5000)
+    end
+end)
+
+RegisterNetEvent('quickcash:client:openMenu', function()
+    menuDeliver()
+end)
+
+RegisterNetEvent('quickcash:client:serverTrigger', function(data)
+    TriggerServerEvent('quickcash:server:remove', data)
+    print("event")
+    itemsGiven = false
+    print("given false")
+    RemoveBlip(currentBlip)
+    currentBlip = nil
+    print("remove blip")
+    drugZone:destroy()
+    print("zone destroy")
+    cooldown()
+    print("cooldown")
+end)
+-- Startup
+local x = false
+CreateThread(function()
+    if not x then
+        createBlips()
+        createTargetPed()
+    end
+end)
+
+-- Security
+RegisterNetEvent('QBCore:Client:OnPlayerUnload', function()
+    if itemsGiven then
+        TriggerServerEvent('quickcash:server:secureRemove', Config.Locations[indx].Item, Config.Locations[indx].Amount)
+    end
+    --print("items removed")
+    inprogress = false
+    --print("not inprogress")
+    if currentBlip ~= nil then
+        RemoveBlip(currentBlip)
+        currentBlip = nil
+    end
+    --print("blip removed")
+    if currentObject ~= nil then
+        DeleteObject(currentObject)
+    end
+    --print("object deleted")
+    finnishedCooldown =  true
+    --print("finished cooldown")
+    drugZone:destroy()
+    --print("zone destroyed")
+end)
---
+++ config.lua
@@ -0,0 +1,140 @@
+Config = Config or {}
+
+Config.Cooldown = 1 -- In minutes
+
+Config.Blip = {
+    location = vector3(22,9,162),
+    scale = .6,
+    color = 15, -- See for refrence https://docs.fivem.net/docs/game-references/blips/
+    sprite = 326, -- See for refrence https://docs.fivem.net/docs/game-references/blips/
+    name = "Test Blip"
+}
+
+Config.StarterPedInfo = {
+    model = `a_m_m_indian_01`, -- https://docs.fivem.net/docs/game-references/ped-models/
+    coords = vector4(1108.06, 58.72, 79.76, 232.27), -- Make sure the Z coordinate is -1 than the original if changing
+
+}
+
+Config.Locations = {
+    -- Coke 
+    [1] = {
+        Item = "cokebaggy2", -- Item name within your shared items folder
+        Amount = 2, -- Amount of items given to the player to transfer between areas
+        Reward = { -- Make sure item, bank, and cash only have 1 true value between either one of them
+            Item = {
+                item = true,
+                name = 'markedbills', -- item given as a reward if item is true
+                amount = 3 -- Amount of items given to player as reward
+            },
+            Money = { -- Decide which account money will go into and the amount that will be given
+                bank = true,
+                cash = true,
+                amount = 10000
+            }
+        },
+        Blip = {
+            coords = vector3(801.06, -726.88, 27.78), -- Location of Blip
+            color = 16, -- See for refrence https://docs.fivem.net/docs/game-references/blips/
+            sprite = 514, -- See for refrence https://docs.fivem.net/docs/game-references/blips/
+            name = "Coke Pickup"  -- Name of blip on map
+        },
+        Prop = {
+            model = 'hei_prop_heist_weed_block_01', -- Ability to change at https://forge.plebmasters.de/objects
+            coords = vector3(800.75, -726.12, 26.82) -- Location, make sure the Z coord is -1 than original
+        },
+        Messages = { -- Messages that Oswald will send during the course of the mission
+            Pickup = "Go pickup the coke brick(s) from the given location",
+            Drop = "Go drop off the coke brick(s) at the given location",
+            Completed = "Excelent work, come see me again soon for more work"
+        }
+    },
+    -- Ammo
+    [2] = {
+        Item = "ammo_box",
+        Amount = 2,
+        Reward = {
+            Item = {
+                item = true,
+                name = 'markedbills',
+                amount = 3
+            },
+            Money = {
+                bank = true,
+                cash = true,
+                amount = 10000
+            }
+        },
+        Blip = {
+            coords = vector3(-261.94, -903.1, 32.31),
+            color = 16,
+            sprite = 150,
+            name = "Ammo Pickup"
+        },
+        Prop = {
+            model = 'prop_box_ammo03a',
+            coords = vector3(-261.94, -903.1, 31.31)
+        },
+        Messages = {
+            Pickup = "Go pickup the ammo box(es) from the given location",
+            Drop = "Go drop off the  ammo box(es) at the given location",
+            Completed = "Excelent work, come see me again soon for more work"
+        }
+    },
+    -- Weed
+    [3] = {
+        Item = "weed_brick2",
+        Amount = 2,
+        Reward = {
+            Item = {
+                item = true,
+                name = 'markedbills',
+                amount = 3
+            },
+            Money = {
+                bank = true,
+                cash = true,
+                amount = 10000
+            }
+        },
+        Blip = {
+            coords = vector3(-3226.18, 1096.84, 10.61),
+            color = 16,
+            sprite = 469,
+            name = "Weed Pickup"
+        },
+        Prop = {
+            model = 'prop_drug_package',
+            coords = vector3(-3226.18, 1096.84, 9.61)
+        },
+        Messages = {
+            Pickup = "Go pickup the weed package(s) from the given location",
+            Drop = "Go drop off the  weed package(s) at the given location",
+            Completed = "Excelent work, come see me again soon for more work"
+        }
+    }
+
+
+}
+
+Config.DropOff = {
+    Blip = {
+        color = 66, -- See for refrence https://docs.fivem.net/docs/game-references/blips/
+        sprite = 271, -- See for refrence https://docs.fivem.net/docs/game-references/blips/
+        name = "Drop Off" -- Name of blip
+    },
+    Locations = { -- Locations which the player will drop off the items
+        [1] = {
+            coords = vector3(-1680.63, -291.25, 51.88),
+
+        },
+        [2] = {
+            coords = vector3(-6.02, -203.52, 52.67),
+
+        },
+        [3] = {
+            coords = vector3(144.08, -130.08, 54.83),
+
+        }
+    }
+}
